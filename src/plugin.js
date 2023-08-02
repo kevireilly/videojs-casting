da68d5eb9610ff8e627a06609d8363e59a1e9d47
Kevin Reilly <kevin@vishun.net>
2023-08-02 00:04:25 -0700

    AirPlay: initial implementation

--- src/plugin.js
+++ src/plugin.js
@@ -1,50 +1,63 @@
-import videojs from 'video.js';
+import VJS from 'video.js';
 import {version as VERSION} from '../package.json';
 
-const Plugin = videojs.getPlugin('plugin');
+const Plugin = VJS.getPlugin('plugin');
 
-// Default options for the plugin.
-const defaults = {};
+const defaults = {
+  airplay: {
+    enabled: true,
+    controlText: 'AirPlay',
+    className: 'vjs-casting-airplay',
+  }
+};
 
-/**
- * An advanced Video.js plugin. For more information on the API
- *
- * See: https://blog.videojs.com/feature-spotlight-advanced-plugins/
- */
 class Casting extends Plugin {
-
-  /**
-   * Create a Casting plugin instance.
-   *
-   * @param  {Player} player
-   *         A Video.js Player instance.
-   *
-   * @param  {Object} [options]
-   *         An optional options object.
-   *
-   *         While not a core part of the Video.js plugin architecture, a
-   *         second argument of options is a convenient way to accept inputs
-   *         from your plugin's caller.
-   */
   constructor(player, options) {
-    // the parent class will add player under this.player
     super(player);
 
-    this.options = videojs.mergeOptions(defaults, options);
+    this.options = VJS.obj?.merge(defaults, options) ?? VJS.mergeOptions(defaults, options);
 
     this.player.ready(() => {
       this.player.addClass('vjs-casting');
+
+      const airPlaySupported = Boolean(window.WebKitPlaybackTargetAvailabilityEvent);
+      if (airPlaySupported && this.options.airplay.enabled) {
+        this.addAirPlayButton();
+      }
+    });
+  }
+
+  addAirPlayButton() {
+    const playerElement = this.player.el();
+    const airPlayElement = playerElement.querySelector('.vjs-casting');
+    const mediaElement = playerElement.querySelector('video, audio');
+    if (airPlayElement || !mediaElement) return;
+
+    const position = this.options.airplay.position ?? this.player.controlBar.children().length;
+    const airPlayButton = this.player.controlBar.addChild('button', {
+      clickHandler: () => {
+        mediaElement?.webkitShowPlaybackTargetPicker();
+      },
+      ...this.options.airplay
+    }, position);
+
+    const handleTargetAvailabilityChanged = ({ availability }) => {
+      if (availability === 'available') {
+        airPlayButton.show();
+      } else {
+        airPlayButton.hide();
+      }
+    }
+
+    mediaElement.addEventListener('webkitplaybacktargetavailabilitychanged', handleTargetAvailabilityChanged);
+    this.on('dispose', function() {
+      mediaElement.removeEventListener('webkitplaybacktargetavailabilitychanged', handleTargetAvailabilityChanged);
     });
   }
 }
 
-// Define default values for the plugin's `state` object here.
 Casting.defaultState = {};
-
-// Include the version number.
 Casting.VERSION = VERSION;
-
-// Register the plugin with video.js.
-videojs.registerPlugin('casting', Casting);
+VJS.registerPlugin('casting', Casting);
 
 export default Casting;
